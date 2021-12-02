7bc0987a99ba89f69c7c8044cae7a964ad57cac0
Opilane TTHK <opilane@tthk.ee>
2021-12-02 10:29:51 +0200

    GAnk

---
+++ MoviesFromFile/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace MoviesFromFile
+{
+    class Program
+    {
+        class Movie
+        {
+            string title;
+            string rateing;
+            string year;
+        }
+        public Movies(string _title, string _rateing, string _year)
+        {
+            title = _title;
+            rateing = _rateing;
+            year = _year;
+        }
+
+        public string Title
+        {
+            get { return title; }
+        }
+        public string Rateing
+        {
+            get { return rateing; }
+        }
+        public void Year
+        {
+            get { return year; }
+        }
+        static void Main(string[] args)
+        {
+            List<string> movielistfromfile = getMoviesFromFile().ToList();
+            List<Movie> lisofMovies = new List<Movie>();
+
+            foreach(string movieRecord in movielistfromfile)
+            {
+                string[] tempArray = movieRecord.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                Movie newMovie = new Movie(tempArray[0], tempArray[1], tempArray[3]);
+
+                lostOfMovies.Add(newMovie);
+            }
+            foreach(Movie movie in lisofMovies)
+            {
+                Console.WriteLine($"Title: {movie.Title} - Rateing: {movie.Rateing} - Year: {movie.Year}");
+            }
+        }
+        public static string[] getMoviesFromFile()
+        {
+            string filepath = @"C:\Users\opilane\samples\movies.txt";
+            string[] datafromfile = File.ReadAllLines(filepath);
+            return datafromfile;
+                
+        }
+  
+        public static void ShowDataFromArray(string[] someArray)
+        {
+            foreach(string line in someArray)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    
+    
+    
+    }   
+    
+}
---
+++ OPPcar/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace OPPcar
+{
+    
+    
+        class Program
+        {
+
+            class Car
+            {
+                string mark;
+                string model;
+                string regNumber;
+                string color;
+                int odo;
+                int fuelTank;
+
+                public Car(string _mark, string _model, string _regNumber, string _color)
+                {
+                    mark = _mark;
+                    model = _model;
+                    regNumber = _regNumber;
+                    color = _color;
+                    odo = 0;
+                    fuelTank = 60;
+
+                    Console.WriteLine($"{color} {mark} {model} {regNumber} has been created.");
+                }
+            }
+            public int fuelTank
+            {
+                get { return fuelTank; }
+            }
+            public int odo
+            {
+                get { return odo; }
+            }
+            public void Drive()
+            {
+                fueltank -= 5;
+                odo += 100;
+                Console.WriteLine("vrooom-Vroom");
+            }
+            public void ShowCarData()
+            {
+                Console.WriteLine($"Mark: {mark}");
+                Console.WriteLine($"Model: {model}");
+                Console.WriteLine($"Registration number: {regNumber}");
+                Console.WriteLine($"Color: {color}");
+                Console.WriteLine($"Odometer: {odo}");
+                Console.WriteLine($"Fuel: {fuelTank}");
+            }
+            static void Main(string[] args)
+            {
+                Car myCar = new Car("Toyota", "Celica", "777BTJ", "Red");
+                while(myCar.FuelTank > 0)
+                {
+                    myCar.Drive();
+
+                }
+                myCar.ShowCarData();
+            }
+        }
+    
+}
